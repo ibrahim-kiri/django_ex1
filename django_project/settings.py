c8e509e3b6fb85ea3db162022e1fc8b32684ec9e
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-05 13:24:29 +0300

    New updates for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%u(kgyd^q^8l=##4y4)blz@^rr3ofj$2o29sa5ixj7imk=zb@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
